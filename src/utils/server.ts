edbb936e105041bbc31e81f8427abeda7c3c04df
xeusteerapat <xeus085@gmail.com>
2022-02-06 13:34:55 +0700

    :recycle: [Update] refactor for testing

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,17 +1,16 @@
 import express from "express";
-import routes from "../routes";
 import deserializeUser from "../middleware/deserializeUser";
+import routes from "../routes";
 
-function createServer() {
+const createServer = () => {
   const app = express();
 
   app.use(express.json());
-
   app.use(deserializeUser);
 
   routes(app);
 
   return app;
-}
+};
 
 export default createServer;
